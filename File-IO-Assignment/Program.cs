900beec69d074142eba171f09d9b57a528d51713
damiansoch <66294165+damiansoch@users.noreply.github.com>
2022-08-21 15:26:06 +0100

    Add project files.

---
+++ File-IO-Assignment/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.IO;
+
+namespace File_IO_Assignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please write the number");
+            string number = Console.ReadLine();
+
+            string path = @"C:\Users\damia\Desktop\Basic_C#_Programs\FileIOAssignment.txt";
+            File.WriteAllText(path, number);
+
+            string readText = File.ReadAllText(path);
+            Console.WriteLine("Text read from the file is {0}", readText);
+
+            Console.ReadKey();
+        }
+
+
+    }
+}
